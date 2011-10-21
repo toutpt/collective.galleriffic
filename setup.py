c1add6f8058f7ac15ebcb349623965b4450c6b20
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-10-21 10:21:25 +0200

    rename README.txt -> rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.galleriffic',
       version=version,
       description="Galleriffic integration into Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
